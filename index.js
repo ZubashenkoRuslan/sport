5130198e0ec450303ae313091f69ccf33743bea6
olenazubashenko <olenazubashenko@gmail.com>
2019-10-03 11:51:01 +0300

    with slider

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 let search = document.getElementById('search');
 let searchShow = document.getElementById('search-show');
 
@@ -5,5 +7,18 @@ search.onclick = function() {
     searchShow.style.background = '#ffffff';  
 }
 
+$('.single-item').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
 
-$('.single-item').slick();
+// $('#slick-slider').on('afterChange', function(event, slick, currentSlide){
+//     $('.btn-wrapper').each((i,e)=>{
+//         e.removeClass('_slider-selected')
+//     });
+//     $('.btn-wrapper')[currentSlide].addClass('_slider-selected')
+
+//     if (currentSlide == 5) { console.log('Осуществлён переход к 5му слайду'); }
+//   });
