93fb387b56ee458c6f202b72e6f3b568d48ceaa9
olenazubashenko <olenazubashenko@gmail.com>
2019-10-03 07:11:40 +0300

    all site done

---
+++ index.js
@@ -0,0 +1,9 @@
+let search = document.getElementById('search');
+let searchShow = document.getElementById('search-show');
+
+search.onclick = function() {
+    searchShow.style.background = '#ffffff';  
+}
+
+
+$('.single-item').slick();
